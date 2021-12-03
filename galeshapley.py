4c6231b92c783d1815d97e30a3672f349c7cc918
Catherine Cui <ccui@college.harvard.edu>
2021-12-03 12:40:43 -0500

    galshapley

--- galeshapley.py
+++ galeshapley.py
@@ -1,10 +1,55 @@
-import sim
+# import sim
 
-# returns true if an agent is a boy
-def isBoy(agent):
-    return agent.id < sim.n
 
 # Input: dict that maps agent ids to full preference profiles, which are lists of agent ids
 # Output: dict that maps agent ids to agent ids of their current match
-def GaleShapley(preferences):
-    #
+
+# male proposing 
+
+class GaleShapley:
+    def __init__(self, preferences):
+        # num of each gender
+        self.num = int(len(preferences)/2)
+
+        # create separate list for men and women 
+        self.men = dict(list(preferences.items())[:len(preferences)//2])
+        self.women = dict(list(preferences.items())[len(preferences)//2:])
+
+        # create a dictionary of pointers for men and women
+        self.menp = [0 for man in self.men.keys()]
+        self.womenp = [self.num for woman in self.women.keys()]
+
+        # create a list for unmatched men and dict for matches
+        self.unmatchedmen = list(self.men.keys())
+        
+        self.matches = {}
+
+    def match(self):
+        while self.unmatchedmen:
+            for man in self.unmatchedmen:
+                # person they're about to propose to 
+                wife = self.men[man][self.menp[man]]
+                self.accept(man, wife)
+                
+        for man in self.men.keys():
+            self.matches[man] = self.men[man][self.menp[man]-1]
+            
+    def accept(self, man, woman):
+        new = self.women[woman].index(man)
+        previous = self.womenp[woman-self.num]
+
+        if new < previous:
+            if previous < self.num:
+                self.unmatchedmen.append(self.women[woman][previous])
+            self.womenp[woman-self.num] = new
+            self.unmatchedmen.remove(man)
+        
+        self.menp[man] += 1
+
+
+
+gs = GaleShapley({0:[3, 4, 5], 1:[4, 3, 5], 2:[5, 3, 4], 3: [0, 2, 1], 4: [2, 0, 1], 5: [0, 2, 1]})
+gs.match()
+
+print(gs.matches)
+
