36873c42633bad4208804e81fe03f3433fdabac5
Lawrence Zhang <lawrencezhang@college.harvard.edu>
2021-12-03 10:32:21 -0500

    Add more structure

--- galeshapley.py
+++ galeshapley.py
@@ -1,9 +1,5 @@
 import sim
 
-# returns true if an agent is a boy
-def isBoy(agent):
-    return agent.id < sim.n
-
 # Input: dict that maps agent ids to full preference profiles, which are lists of agent ids
 # Output: dict that maps agent ids (boys) to agent ids (girls) of their current match
 def GaleShapley(preferences):
--- sim.py
+++ sim.py
@@ -1,5 +1,6 @@
 import random
 import galeshapley
+from collections import defaultdict
 
 # number of agents
 n = 1000
@@ -11,22 +12,29 @@ x = 20
 # number of following systematic elo rounds
 y = 100
 
+
 class Agent:
     def __init__(self, id_num):
         # each agent has random attributes that can take each take on 5 values
         self.attributes = [random.choice(['A', 'B', 'C', 'D', 'F']) for _ in range(m)]
 
         # list of 10 lists with A, B, C, D, F in some order
+        # TODO: Charu
         self.attribute_preferences = [[]]
 
         # list of 10 randomly generated weights that sum to 1
         self.weights = [random.uniform(0, 1) for _ in range(m)]
         self.weights = [x / sum(self.weights) for x in self.weights]
 
+        # dict mapping agent id to this agent's score for that agent
+        # TODO: Charu
+        self.scores = {}
+
         # unique id of agent
         self.id = id_num
         # starting elo of agent
         self.elo = 400
+
 
 # Input: list of agents
 # Output: dict that maps agent ids to true full preference profiles, which are lists of agent ids
@@ -34,11 +42,29 @@ def generate_true_preference_profile(agents):
     # TODO: Charu
     pass
 
+
 # Input: list of agents
 # Output: dict that maps agent ids to estimated full preference profiles, which are lists of agent ids
 def elo(agents):
-    # TODO: Lawrence
-    pass
+    # ensure each agent paired with different agent every round
+    seen = defaultdict(set)
+    # initial random elo rounds
+    for _ in range(x):
+        # stores results of round (maps agent id to (win/loss, other agent's elo))
+        results = {}
+        # every boy has his profile shown to a randomly chosen girl
+        for i in range(n):
+            # randomly choose agent j that sees agent i's profile
+            j = random.randint(n, 2 * n - 1)
+            while (j in seen[i]):
+                j = random.randint(n, 2 * n - 1)
+            seen[i].add(j)
+        # every girl has her profile shown to a randomly chosen boy
+            
+            
+    # following systematic elo rounds
+    # for _ in range(y):
+
 
 
 def main():
@@ -46,7 +72,7 @@ def main():
     agents = [Agent(i) for i in range(2 * n)]
 
     # Generate agents' true full preference profile 
-    # and call Gale-Shapley algorithm
+    # and call boy-proposing Gale-Shapley algorithm
     true_preference_profiles = generate_true_preference_profile(agents)
     true_matches = galeshapley.GaleShapley(true_preference_profiles)
 
